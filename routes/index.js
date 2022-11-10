ff0f6cd17058c4765a38ddb739fe7211c67edbd8
Nacho35 <ignacioevil@gmail.com>
2022-11-10 17:55:02 -0300

    estructura de proyecto creada

---
+++ routes/index.js
@@ -0,0 +1,6 @@
+// const express = require("express");
+// const routes = express.Router();
+
+// const { userController, productController } = require("../controllers");
+// const { isAuth } = require("../middlewares");
+// const { userSchema } = require("../controllers/schemas");
