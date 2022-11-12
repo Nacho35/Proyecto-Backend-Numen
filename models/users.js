50a25687fd838fe35cb9c79dc1ba32004def60f3
Nacho35 <ignacioevil@gmail.com>
2022-11-11 22:19:49 -0300

    encriptacion de clave de usuario en mongo y register terminado falta login

--- models/users.js
+++ models/users.js
@@ -1,3 +1,4 @@
+const { hash } = require("bcrypt-nodejs");
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
@@ -20,6 +21,21 @@ const userSchema = new Schema({
     type: Date,
     default: Date.now(),
   },
+});
+userSchema.pre("save", function (next) {
+  let user = this;
+  bcrypt.genSalt(10, (error, salt) => {
+    if (error) {
+      return next(error);
+    }
+    bcrypt.hash(user.password, salt, null, (error, hash) => {
+      if (error) {
+        return next(error);
+      }
+      user.password = hash;
+      next();
+    });
+  });
 });
 
 module.exports = mongoose.model("Users", userSchema);
