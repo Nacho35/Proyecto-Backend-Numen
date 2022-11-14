c7dd31380bb17181f7bb285f0bcca3d84f7ff4ba
Nacho35 <ignacioevil@gmail.com>
2022-11-14 20:34:52 -0300

    problema en userService

--- services/userService.js
+++ services/userService.js
@@ -1,5 +1,6 @@
 const authService = require("./authService");
 const User = require("../models/users");
+const newUser = require("../controllers/userController");
 
 const login = (email, password) => {
   return new Promise((resolve, reject) => {
@@ -7,7 +8,7 @@ const login = (email, password) => {
       if (error) {
         reject({
           status: 500,
-          message: "Se produjo un error al registrar el usuario.",
+          message: "Se produjo un error al iniciar sesión.",
           error,
         });
       }
@@ -27,9 +28,43 @@ const login = (email, password) => {
   });
 };
 
-const register = () => {};
+const register = () => {
+  return new Promise((resolve, reject) => {
+    User.findOne({ email: newUser.email }, (error, user) => {
+      if (error) {
+        reject({
+          status: 500,
+          message: "Se produjo un error al registrar el usuario.",
+          error,
+        });
+      }
+      if (user) {
+        reject({
+          status: 400,
+          message: "El email ya se encuentra en uso.",
+        });
+      }
+      newUser.save((error) => {
+        if (error) {
+          reject({
+            status: 400,
+            message: "Se produjo un error al registrar el usuario.",
+            error,
+          });
+        }
+        resolve({
+          status: 201,
+          message: "El registro se completo exitosamente",
+          token: authService.createToken(),
+        });
+      });
+    });
+  });
+};
 
 module.exports = {
   login,
   register,
 };
+
+// PROBLEMA EN EL  newUser.save SERVER DICE IS NOT A FUNCTION
